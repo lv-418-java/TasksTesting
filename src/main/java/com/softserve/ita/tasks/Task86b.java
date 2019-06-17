84a692832d757d1068f387cc97c1a264e01ba8be
marynaMelnichuk <45080844+marynaTCHAN@users.noreply.github.com>
2019-06-17 17:32:38 +0300

    Add files via upload

---
+++ src/main/java/com/softserve/ita/tasks/Task86b.java
@@ -0,0 +1,33 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+
+
+public class Task86b implements Task<Integer> {
+    private final static Task86b task86b = new Task86b();
+
+    public synchronized static Task<Integer> getInstance(){
+      return task86b;
+    }
+
+    private Task86b(){
+
+    }
+
+    private int sumOfNumbers;
+    private int naturalNumber;
+
+    @Override
+    public void setUp(BufferedReader reader) throws IOException {
+        System.out.println("Enter natural number:");
+        naturalNumber = Integer.valueOf(reader.readLine());
+    }
+
+    @Override
+    public Integer execute() {
+        char array[] = Task86a.returnCharArrayOfNumber(naturalNumber);
+        for(int i = 0; i < array.length; i++ ) {
+            sumOfNumbers += Character.getNumericValue(array[i]);
+        }
+        return sumOfNumbers;
+    }
+}
